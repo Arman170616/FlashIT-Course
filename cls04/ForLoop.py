2ef6471ff1da48cce0eb634be4b5bcf07441a091
Arman170616 <armanice09@gmail.com>
2023-05-23 21:52:27 +0600

    while loop break statement added

--- cls04/ForLoop.py
+++ cls04/ForLoop.py
@@ -6,17 +6,3 @@
 #     print(x)
 
 
-# count = 0
-# while count <5:
-#     print(count)
-#     count = count +1
-
-
-password = ""
-
-while password != "python":
-    password = input("Enter password: ")
-    if password == "python":
-        print("You are logged in")
-    else:
-        print("Sorry, try again")
---
+++ cls04/whileLoop.py
@@ -0,0 +1,27 @@
+# count = 0
+# while count <5:
+#     print(count)
+#     count = count +1
+
+
+# password = ""
+
+# while password != "python":
+#     password = input("Enter password: ")
+#     if password == "python":
+#         print("You are logged in")
+#     else:
+#         print("Sorry, try again")
+
+
+i = 0
+while i < 6:
+    print(i)
+    if i == 3:
+        print('Breaking from loop')
+        break
+    i += 1
+
+# 1. init
+# 2. condition
+# 3. increment/decrement
