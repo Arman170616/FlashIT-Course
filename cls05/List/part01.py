765e852961782353baa2b5710545cb553c12b7ae
Arman170616 <armanice09@gmail.com>
2023-05-26 22:09:48 +0600

    cls05/List

---
+++ cls05/List/part01.py
@@ -0,0 +1,55 @@
+# list 
+#ordered
+#changeable
+#allow duplicates
+
+list1 = [1, 2, 3, 4, 5, 2]
+# print(type(a))
+# print(a)
+# a.sort()
+# print(a)
+# a.reverse()
+# print(a)
+
+# print(a[3])
+
+#cheack if item exists
+
+# if 2 in list1:
+#     print('yes')
+
+#change item value
+
+# list1[2] = 10
+
+# print(list1)
+
+#insert item
+
+# list1.insert(0, 9)
+# print(list1)
+
+
+#append item
+
+list1.append(10)
+# print(list1)
+
+#remove item
+# list1.remove(10) #remove by 10
+# print(list1)
+
+list2 = [1, 2, 3, 4, 5, 2]
+
+# list1.extend(list2)
+# print(list1)
+
+# list1.pop(2) #remove by index
+# print(list1)
+
+#delete list
+# del list1
+# print(list1)
+
+# list1.clear()
+# print(list1)
