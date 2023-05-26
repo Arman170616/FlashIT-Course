8d8b2b976ce89380ce4926256beb20f152364358
Arman170616 <armanice09@gmail.com>
2023-05-26 21:37:56 +0600

    cls05/String

---
+++ cls05/String/part01.py
@@ -0,0 +1,26 @@
+txt = "Hello I am from Bangladesh"
+# print('Bangladesh' not in txt)
+
+
+#slice string
+
+a = "Hello, World!"
+# print(len(a))
+# print(a[2:5])
+# print(a[:5])
+# print(a[2:])
+# print(a[-5:-2])
+
+# modify string
+
+c = " Hello World! "
+# print(c.upper())
+# print(c.lower())
+# print(c.strip())
+# print(c.replace("H", "J"))
+# print(c.split(",")) # returns ['Hello', ' World!']
+
+
+z = 'hell'
+print(z.zfill(15))
+
