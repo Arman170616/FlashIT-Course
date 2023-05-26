286292d9c2f83a12a4531f639c57c2b7d47e44fc
Arman170616 <armanice09@gmail.com>
2023-05-26 22:20:02 +0600

    list comprehension

--- cls05/List/part01.py
+++ cls05/List/part01.py
@@ -53,3 +53,24 @@ list2 = [1, 2, 3, 4, 5, 2]
 
 # list1.clear()
 # print(list1)
+
+
+#loop through list
+# for x in list1:
+#     print(x)
+
+
+#list comprehension
+
+# [print(x) for x in list1]
+
+
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+a = [x for x in a if x < 5]
+print(a)
+
+#join two list
+
+list3 = list1 + list2
+print(list3)
